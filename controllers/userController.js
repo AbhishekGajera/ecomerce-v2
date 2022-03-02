d4dd2879f40577c90945f3b492a345e28cd1bb89
AbhishekGajera <abhishekgajera3140@gmail.com>
2022-03-02 22:40:43 +0530

    remove: heroku cli

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,8 +8,8 @@ const cloudinary = require("cloudinary");
 
 // Register a User
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-  // const myCloud = await cloudinary.v2.uploader.upload(req.files.avatar.tempFilePath, {
-    const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+  const myCloud = await cloudinary.v2.uploader.upload(req.files.avatar.tempFilePath, {
+    // const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
     folder: "avatars",
     width: 150,
     crop: "scale",
