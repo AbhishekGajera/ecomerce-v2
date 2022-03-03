87055ef265d8d1bb2b5647403a54bdf4fccd5ef7
AbhishekGajera <abhishekgajera3140@gmail.com>
2022-03-03 10:09:54 +0530

    fix: cors origin

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 const path = require("path");
+const cors = require('cors')
 
 const errorMiddleware = require("./middleware/error");
 
@@ -12,6 +13,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "config/config.env" });
 }
 
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
